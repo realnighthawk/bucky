48d349c316d0698dd0f7c45b7ea53496840088aa
kumarabd <abishekkumar@Abisheks-MacBook-Pro.local>
2020-02-10 13:24:24 +0530

    added logger,utils and errors

--- utils/error.go
+++ utils/error.go
@@ -6,6 +6,10 @@ func ErrUnmarshal(key string, err error) error {
 	return errors.New("ERR.UNMARSHAL", "Unmarshal error for key: "+key+", error: "+err.Error())
 }
 
+func ErrMarshal(err error) error {
+	return errors.New("ERR.MARSHAL", "Marshal error, Description: "+err.Error())
+}
+
 func ErrGetBool(key string, err error) error {
 	return errors.New("ERR.GETBOOL", "Error while getting Boolean value for key: "+key+", error: "+err.Error())
 }
