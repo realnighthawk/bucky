aa0d983a70ff23c4a808345260534e6467073d11
kumarabd <abishekkumar@Abisheks-MacBook-Pro.local>
2020-02-10 13:42:02 +0530

    refactored logger

--- logger/logger.go
+++ logger/logger.go
@@ -19,28 +19,28 @@ type Logger struct {
 }
 
 func New(appname string) (*LogHandler, error) {
-	l = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	l := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 	l = log.With(l, "ts", time.Now().String())
 	l = log.With(l, "appname", appname)
-	return &Logger{logger: l}
+	return &Logger{logger: l}, nil
 }
 
 func (l *Logger) Err(location string, code string, des string) {
-	l = log.With(l, "level", "ERROR")
+	l = log.With(l.logger, "level", "ERROR")
 	l.Log("reference", location, "code", code, "description", des)
 }
 
 func (l *Logger) Info(des string) {
-	l = log.With(l, "level", "INFO")
+	l = log.With(l.logger, "level", "INFO")
 	l.Log("description", des)
 }
 
 func (l *Logger) Warn(location string, des string) {
-	l = log.With(l, "level", "WARN")
+	l = log.With(l.logger, "level", "WARN")
 	l.Log("reference", location, "description", des)
 }
 
 func (l *Logger) Debug(des string) {
-	l = log.With(l, "level", "DEBUG")
+	l = log.With(l.logger, "level", "DEBUG")
 	l.Log("description", des)
 }
