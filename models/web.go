9ee0e2fb1d56535e51bc98c044791b921957a5fc
kumarabd <abishekkumar92@gmail.com>
2020-07-31 17:27:24 +0530

    added stats

--- models/web.go
+++ models/web.go
@@ -12,6 +12,14 @@ type Response struct {
 
 type Health struct {
 	Version string `json:"version"`
-	Status  string `json"status"`
+	Status  string `json:"status"`
 	Error   string `json:"error"`
+}
+
+type Stats struct {
+	Name      string    `json:"name"`
+	Port      string    `json:"port"`
+	Proxy     string    `json:"proxy"`
+	Version   string    `json:"version"`
+	StartedAt time.Time `json:"startedat,string"`
 }
