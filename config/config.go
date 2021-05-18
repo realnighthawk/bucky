3d18964acbba9006bc37174751acc1b06ce40a89
kumarabd <abishekkumar92@gmail.com>
2021-05-18 14:01:42 +0530

    changed comments
    Signed-off-by: kumarabd <abishekkumar92@gmail.com>

--- config/config.go
+++ config/config.go
@@ -1,18 +1,15 @@
 // Package config provides the interface Handler and errors related to the configuration of adapters.
 package config
 
-// Provided implementations can be found in the package config/provider.
+// Handler is the config interface to fascilitate multiple providers
 type Handler interface {
 	// SetKey is used to set a string value for a given key.
 	SetKey(key string, value string)
-
 	// GetKey is used to retrieve a string value for a given key.
 	GetKey(key string) string
 
 	// GetObject is used to retrieve an object for a given key and a given interface representing that object in result.
-	// An example of such an object is map[string]string. These objects can e.g. be set in the factory function for a specific
-	// config provider implementation.
 	GetObject(key string, result interface{}) error
-
+	// SetObject is used to set an object for a given key and a given interface representing that object in result.
 	SetObject(key string, value interface{}) error
 }
